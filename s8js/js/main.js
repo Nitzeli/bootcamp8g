c5309eb127dfb2f28fce7354d98657cbd8899ff5
Nitzeli <ntzeli_alejandra@hotmail.com>
2020-05-25 22:48:27 -0500

    primera practica js

---
+++ s8js/js/main.js
@@ -0,0 +1,8 @@
+var numberOne= 50
+var numberTwo= 20
+
+console.log("La suma de los números uno y dos da como resultado", "=",numberOne+numberTwo)
+console.log("La resta de los números uno y dos da como resultado","=",numberOne-numberTwo)
+console.log("La multiplicación de los números uno y dos da como resultado","=",numberOne*numberTwo)
+console.log("La división entre los números uno y dos da como resultado", "=",numberOne/numberTwo)
+console.log("El módulo de los números uno y dos da como resultado", "=",numberOne%numberTwo)
